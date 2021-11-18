6ba5b02939ec9d01a4933324c0b0995800e6eb2f
tatsuji_ho <longer271271@gmail.com>
2021-11-18 16:54:38 +0900

    marged and add me

--- intern/src/components/Overview.vue
+++ intern/src/components/Overview.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>This page is overview.</h1>
+    <h1>インターン紹介</h1>
+    <!--<h1>This page is overview.</h1>
     <div>
       <table>
         <th>
@@ -25,7 +26,36 @@
           </button>
         </tr>
       </table>
+    </div>-->
+    <b-container  class="bv-example-row" fluid="md">
+    <div class="row">
+        <div class="col-8"></div>
+        <div class="col-4" align="right">
+            <input type="text" class="form-control" id="search_input" v-model="keyword" placeholder="名前で検索">
+        </div>
     </div>
+    <br>
+      <b-row>
+      <b-col class="d-flex" v-for="user in filteredusers" :key="user.id" md=3 style="margin-bottom:10px">
+        <b-card class="flex-fill">
+          <b-card-body style="padding: 0 0 0 0;">
+            <p style="visibility:hidden; height:0px;">{{user.id}}</p>
+            <img :src="'./img/'+user.facephoto" class="card-img-top" alt="画像" style="margin-bottom:10px;">
+          <b-card-title>{{user.name}}</b-card-title>
+          <b-card-text style="white-space: pre-line; height:50px">
+          {{user.samune}}
+          </b-card-text>
+          <button
+            type="is-info"
+            @click="showMoreInformation" class="btn btn-primary">
+            詳細を見る
+          </button>
+          </b-card-body>
+        </b-card>
+      </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
@@ -37,6 +67,7 @@ if(typeof(String.prototype.trim) === "undefined")
         return String(this).replace(/^\s+|\s+$/g, '');
     };
 }
+import axios from 'axios'
 
 export default {
   name: 'Detail',
@@ -44,37 +75,40 @@ export default {
     // 返却するオブジェクト users は本コンポーネントで表示するユーザ情報
     // 本来ならば DB 等で保持するのだが、今回は記事用のサンプルコードということでリストで持たせている
     return {
-      users: [
-        {
-          name: 'hogehoge',
-          live: 'Japan Tokyo'
-        },
-        {
-          name: 'barbar',
-          live: 'Japan Kanagawa'
-        },
-        {
-          name: 'piypiyo',
-          live: 'Japan Kanagawa'
-        },
-        {
-          name: 'fugafuga',
-          live: 'Japan Chiba'
-        },
-        {
-          name: 'varvar',
-          live: 'Japan Saitama'
-        }
-      ],
+      keyword: '',
+      users: []
     }
+  },
+  mounted: function(){
+    axios.get('./data/users.json')
+    .then(function(response){
+        this.users = response.data.users
+        console.log(this.users)
+    }.bind(this))
+    .catch(function(error){
+        console.log(error)
+    })
   },
   methods: {
     showMoreInformation: function(event) {
-      const id = Number(event.path[1].childNodes[0].innerText.trim())
+      console.log(event)
+      const id = Number(event.path[1].childNodes[2].textContent.trim())
       this.$router.push({
         name: 'detail',
-        params: { id: id + 1 }
+        params: { id: id }
       })
+    }
+  },
+  computed:{
+    filteredusers: function() {
+      var tmpusers = [];
+      for(var i in this.users) {
+        var user = this.users[i];
+        if(user.name.indexOf(this.keyword) !== -1) {
+          tmpusers.push(user);
+        }
+      }
+      return tmpusers;
     }
   }
 }
--- intern/src/main.js
+++ intern/src/main.js
@@ -4,6 +4,12 @@ import App from './App.vue'
 // ルーティングのために追加
 import router from './router'
 
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
 new Vue({
